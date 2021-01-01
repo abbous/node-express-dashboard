4d337d7263aa8f80034a4fbb9a9e9cc30ec46d24
abbous <egrsyriano@gmail.com>
2020-12-31 17:55:30 -0800

    Update log-viewer.js

--- public/javascripts/log-viewer.js
+++ public/javascripts/log-viewer.js
@@ -2,11 +2,13 @@
 
     const connection = new WebSocket("ws://localhost:3000")
 const logWindow = document.querySelector("#log-window")
+const filePath= document.getElementById('#logFilePath')
 
 connection.onopen = () => {
   connection.send("Hello from the client!")
 }
 
 connection.onmessage = (event) => {
+
   logWindow.innerHTML = event.data
 }
