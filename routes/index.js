f0801b79d4ff97ee52125d757beabac7fc0da69c
abbous <egrsyriano@gmail.com>
2020-12-31 17:55:19 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const { body } = require("express-validator");
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
-  res.render("index", { title: "Log Dashboard" });
+  res.render("index", {logFile:req.query.logFile});
 });
 
 /* GET select file. */
