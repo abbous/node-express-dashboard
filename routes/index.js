b574e594cb038137a7935a5257b35f5e0e3cce8b
abbous <egrsyriano@gmail.com>
2021-01-05 14:30:53 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const fileService = require("../services/select-file-service")
-const { getSettings, writeSettings, isValidDir } = require("../services/settings-service.js");
+const { getSettings, writeSettings, isValidDir,getDefaultDir } = require("../services/settings-service.js");
 const { validationResult } = require("express-validator");
 const { body } = require("express-validator");
-const  {getDefaultDir} = require('../services/settings-service.js')
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
